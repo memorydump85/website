533d743d98d2dda9cba572d8305890e39f6d16ac
Pradeep Ranganathan <rpradeep@bitbucket.(none)>
2010-12-15 02:53:00 -0500

    Final touches

--- gallery/import-images.py
+++ gallery/import-images.py
@@ -14,9 +14,9 @@ pattern = '*.JPG'
 
 XML = open('gallery.xml', 'w');
 
-XML.write( '<simpleviewerGallery maxImageHeight="2048" maxImageWidth="2048" \
+XML.write( '<simpleviewerGallery maxImageHeight="550" maxImageWidth="1024" \
 textColor="0xFFFFFF" frameColor="0xffffff" frameWidth="20" stagePadding="40" \
-thumbnailColumns="2" thumbnailRows="5" navPosition="left" title="Winter 2010" \
+thumbnailColumns="2" thumbnailRows="4" navPosition="left" title="Winter 2010" \
 enableRightClickOpen="true" backgroundImagePath="" thumbPath="thumbs/" \
 imagePath="images/" >\n' )
 
@@ -31,13 +31,13 @@ for root, dirs, files in os.walk(rootPath):
             # generate thumbnails and links
             absFileName = os.path.join(root, filename)
             print absFileName
-            os.system('sudo convert ' + absFileName + ' -auto-orient -resize x600 images/' + filename)
+            os.system('sudo convert ' + absFileName + ' -auto-orient -resize x550 images/' + filename)
             os.system('sudo convert ' + absFileName + ' -auto-orient -resize x80 thumbs/' + filename)
             
             #generate xml content
             XML.write( '<image>\n' )
             XML.write( '    <filename>' + filename + '</filename>\n' )
-            XML.write( '<caption> </caption>\n' )
+            XML.write( '    <caption>' + os.popen('exiv2 -pc ' + absFileName).read() + '</caption>\n' )
             XML.write( '</image>\n')
 
 print 'Finished generating gallery.xml'
