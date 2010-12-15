6cab5e8b691cdc0bb130e83e593352c373415041
Pradeep Ranganathan <rpradeep@bitbucket.(none)>
2010-12-14 23:37:41 -0500

    Replace e2 gallery with imago

--- gallery/import-images.py
+++ gallery/import-images.py
@@ -9,17 +9,33 @@ os.system('sudo rm -f imagethumbs/*')
 
 rootPath = '/home/rpradeep/Pictures/Photos'
 pattern = '*.JPG'
- 
-print "Copying new images ...sudo r"
+
+XML = open('gallery.xml', 'w');
+
+XML.write( '<simpleviewerGallery maxImageHeight="1024" maxImageWidth="1024"\
+textColor="0xFFFFFF" frameColor="0xffffff" frameWidth="20" stagePadding="40"\
+thumbnailColumns="3" thumbnailRows="3" navPosition="left" title="Madeira"\
+enableRightClickOpen="true" backgroundImagePath="" thumbPath="thumbs/"\
+imagePath="images/" >\n' )
+
+
+print "Copying new images ..."
 for root, dirs, files in os.walk(rootPath):
     for filename in fnmatch.filter(files, pattern):
         if root.endswith('.extra'):
         # ignore images in the .extra directories
             continue
         else:
+            # generate thumbnails and links
             absFileName = os.path.join(root, filename)
             print absFileName
-            os.system('sudo convert ' + absFileName + ' -resize 20% images/' + filename)
-            os.system('sudo convert ' + absFileName + ' -resize 5% imagethumbs/' + filename)
+            os.system('sudo cp -l ' + absFileName + ' images/' + filename)
+            os.system('sudo convert ' + absFileName + ' -resize 5% thumbs/' + filename)
+            
+            #generate xml content
+            XML.write( '<image>\n' )
+            XML.write( '    <filename>' + filename + '</filename>\n' )
+            XML.write( '</image>\n')
 
-print 'Done'
+print 'Finished generating gallery.xml'
+XML.write( '</simpleviewerGallery>\n' )
